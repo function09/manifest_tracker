b016c6b8bb708f6c827de169ecabb6a2e4ac5589
houssine <houssine199222@gmail.com>
2024-01-28 23:01:15 -0500

    Set up database conneciton

--- app/index.js
+++ app/index.js
@@ -1,11 +1,27 @@
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
 import router from "./routes/routes.js";
+import "dotenv/config";
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use("/", router);
+app.use("/v1/manifests", router);
+
+// MongoDB connection
+const { CONNECTION_STRING } = process.env;
+
+mongoose.set("strictQuery", true);
+
+async function main() {
+  await mongoose.connect(CONNECTION_STRING);
+}
+
+main().catch((error) => {
+  console.log(error);
+});
+
 export default app;
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,7 +3,19 @@ import Router from "express";
 const router = Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Hello World");
+  res.send("GET route to display all manifests");
+});
+
+router.post("/upload", (req, res, next) => {
+  res.send("POST route to create a new manifest object");
+});
+
+router.put("/update/:id", (req, res, next) => {
+  res.send("PUT route to update manifest info");
+});
+
+router.delete("/delete/:id", (req, res, next) => {
+  res.send("DELETE route to delete manifest info");
 });
 
 export default router;
