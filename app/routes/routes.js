db4867aafec46f346b03144d949e51a5505227c9
houssine <houssine199222@gmail.com>
2024-02-07 00:42:42 -0500

    Create a new route to display a single document

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -11,6 +11,10 @@ router.get("/", (req, res, next) => {
   res.send("GET route to display all manifests");
 });
 
+router.get("/:id", (req, res) => {
+  res.send("GET route to display a single manifest");
+});
+
 router.post("/upload", upload.single("file"), uploadFile);
 
 router.put("/update/:id", (req, res, next) => {
