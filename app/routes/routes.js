985a9cc1a9dfb31a703f807ce552235acaf1acae
houssine <houssine199222@gmail.com>
2024-02-09 00:11:14 -0500

    Create route function to delete manifests

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,8 @@
 import Router from "express";
 import multer from "multer";
 import uploadFile from "../controllers/uploadManifest.js";
-import { getAllDocumentsJSON, getSingleDocumentJSON } from "../controllers/displayDocuments.js";
+import { deleteDocument, getAllDocumentsJSON, getSingleDocumentJSON } from "../controllers/displayDocuments.js";
+import { deleteManifest } from "../services/databaseFunctions.js";
 
 const router = Router();
 
@@ -14,12 +15,15 @@ router.get("/:id", getSingleDocumentJSON);
 
 router.post("/upload", upload.single("file"), uploadFile);
 
+/* 
+Typically only need to keep track of the material doc number 
+after submitting manifests, no need to update everything else
+*/
+
 router.put("/update/:id", (req, res, next) => {
   res.send("PUT route to update manifest info");
 });
 
-router.delete("/delete/:id", (req, res, next) => {
-  res.send("DELETE route to delete manifest info");
-});
+router.delete("/delete/:id", deleteDocument);
 
 export default router;
