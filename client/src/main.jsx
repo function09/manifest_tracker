3db62ac27ce3ffe6e29968be908bdae9e0941660
houssine <houssine199222@gmail.com>
2024-03-15 23:44:09 -0400

    Setup UI5 Web Components for React

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {ThemeProvider} from "@ui5/webcomponents-react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider>
   <React.StrictMode>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ThemeProvider>
 )
