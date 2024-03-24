4f4af25a9f390d8090fc84be39474af670ab5815
houssine <houssine199222@gmail.com>
2024-03-24 01:55:28 -0400

    Clean up components

--- client/src/components/AnalyticalTable.jsx
+++ client/src/components/AnalyticalTable.jsx
@@ -1,143 +1,159 @@
-import "@ui5/webcomponents-icons/dist/AllIcons.js";
-import { useEffect, useState } from "react"
-import {AnalyticalTable, FlexBox, Button, FileUploader} from "@ui5/webcomponents-react"
-import { ManifestDialog } from "./Dialogs";
+import '@ui5/webcomponents-icons/dist/AllIcons.js';
+import { useEffect, useState } from 'react';
+import { AnalyticalTable, FlexBox, Button, FileUploader } from '@ui5/webcomponents-react';
+import { ManifestDialog } from './Dialogs';
 
 export default function DocumentTable() {
-    const [data, setData] = useState([])
-    const [message, setMessage] = useState("")
-    const [isEmpty, setIsEmpty] = useState(true)
-    const [display, setDisplay] = useState(false)
-    const [error, setError] = useState(null)
+  const [data, setData] = useState([]);
+  const [message, setMessage] = useState('');
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [display, setDisplay] = useState(false);
+  const [error, setError] = useState(null);
 
-    // Add a loading modal to display as requests are being sent and processed by server
-    const fetchManifests = async () => {
-        try {
-          const response = await fetch('http://localhost:3000/api/v1/manifests');
-      
-          if (!response.ok) {
-            const message = `An error has occured: ${response.status}`;
-            throw new Error(message);
-          }
-      
-          const manifests = await response.json();
-          const result = manifests.message;
+  function displayDialog() {
+    if (display === true) setDisplay(false);
+  }
 
-          if(typeof result === "string"){
-            setData([])
-            setMessage(result)
-            setDisplay(true)
-          } else{
-            setData(result)
-            setIsEmpty(false)
-            setDisplay(false)
-          }
-        } catch (error) {
-          setError(error)
-        }
-      };
+  // Add a loading modal to display as requests are being sent and processed by server
+  const fetchManifests = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/v1/manifests');
 
-    useEffect(()=> {
-          fetchManifests()
-    }, [])
+      if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+      }
 
-    
-    /*
+      const manifests = await response.json();
+      //   Fix the message that displays
+      const result = manifests.message;
+
+      if (typeof result === 'string') {
+        setData([]);
+        setMessage(result);
+        setDisplay(true);
+      } else {
+        setData(result);
+        setIsEmpty(false);
+        setDisplay(false);
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchManifests();
+  }, []);
+
+  /*
     Select the file, create a new form data object, 
     use the form data object to send the file to the server for processing
-    */
-    const uploadManifest = async (event) => {
-        const file = event.target.files[0]
+  */
 
-        if(!file) return 
+  const uploadManifest = async (event) => {
+    const file = event.target.files[0];
 
-        const formData = new FormData()
-        
-        // consider changing "file" to "manifest" in both server-side and client
-        formData.append("file", file)
+    if (!file) return;
 
-        const fetchOptions = {
-            method: "POST",
-            body: formData,
-        }
+    const formData = new FormData();
 
-        try{
-            const response = await fetch("http://localhost:3000/api/v1/manifests/upload", fetchOptions)
+    // consider changing "file" to "manifest" in both server-side and client
+    formData.append('file', file);
 
-            if(!response.ok){
-                const result = await response.json()
-                const errorMessage = result.message
-                setMessage(errorMessage)
-                setDisplay(true)
-            } else{
-                setMessage("")
-                fetchManifests()
-                setDisplay(false)
-            }
-           
-        }catch(error){
-            setError(error)
-        }     
+    const fetchOptions = {
+      method: 'POST',
+      body: formData,
+    };
+
+    try {
+      const response = await fetch('http://localhost:3000/api/v1/manifests/upload', fetchOptions);
+
+      if (!response.ok) {
+        const result = await response.json();
+        const errorMessage = result.message;
+        setMessage(errorMessage);
+        setDisplay(true);
+      } else {
+        setMessage('');
+        fetchManifests();
+        setDisplay(false);
+      }
+    } catch (error) {
+      setError(error);
     }
+  };
 
-    const tableColumns = [
-            {
-                Header: 'Document Number',
-                accessor: 'documentNumber',
-                headerTooltip: 'Document Number'
-            },
-            {
-                Header: 'Material Document',
-                accessor: 'materialDocument',
-                headerTooltip: 'Material Document'
-            },
-            {
-                Header: 'Sending Warehouse',
-                accessor: 'sendingWarehouse',
-                headerTooltip: 'Sending Warehouse'
-            },
-            {
-                Header: 'Departure Date',
-                accessor: 'departureDate',
-                headerTooltip: 'Departure Date'
-            },
-            {
-                Header: 'Arrival Date',
-                accessor: 'arrivalDate',
-                headerTooltip: 'Arrival Date'
-            },
-            {
-                Header: 'Actions',
-                accessor: '.',
-                headerTooltip: 'actions',
-                Cell: () => {
-                    return(
-                        <FlexBox>
-                            <Button icon="edit"/>
-                            <Button icon="delete"/>
-                            <Button icon= "activity-items"/>
-                        </FlexBox>
-                    )
-                }
-            }
-    ]
+  const tableColumns = [
+    {
+      Header: 'Document Number',
+      accessor: 'documentNumber',
+      headerTooltip: 'Document Number',
+    },
+    {
+      Header: 'Material Document',
+      accessor: 'materialDocument',
+      headerTooltip: 'Material Document',
+    },
+    {
+      Header: 'Sending Warehouse',
+      accessor: 'sendingWarehouse',
+      headerTooltip: 'Sending Warehouse',
+    },
+    {
+      Header: 'Departure Date',
+      accessor: 'departureDate',
+      headerTooltip: 'Departure Date',
+    },
+    {
+      Header: 'Arrival Date',
+      accessor: 'arrivalDate',
+      headerTooltip: 'Arrival Date',
+    },
+    {
+      Header: 'Actions',
+      accessor: '.',
+      headerTooltip: 'actions',
+      Cell: () => {
+        return (
+          <FlexBox>
+            <Button icon="edit" />
+            <Button icon="delete" />
+            <Button icon="activity-items" />
+          </FlexBox>
+        );
+      },
+    },
+  ];
 
-    return (
-        <>
-         <>
-            {error && <ManifestDialog display={display} message={message} upload={uploadManifest}/>}
-            {data.length === 0 && <ManifestDialog display={display} message={message} upload={uploadManifest}/>
-            } 
-            {data.length > 0 && !error && (
-                <>
-                    <ManifestDialog display={display} message={message} upload={uploadManifest}/>
-                    <AnalyticalTable columns={tableColumns} data={data}/>
-                    <FileUploader onChange={uploadManifest} hideInput>
-                        <Button>Upload single file</Button>
-                    </FileUploader>
-                </>
-            )}
-        </>
-        </>
-    )
+  //   if (error) {
+  //     return <ManifestDialog display={display} message={message} upload={uploadManifest} />;
+  //   }
+
+  //   if (data.length === 0) {
+  //     return <ManifestDialog display={display} message={message} upload={uploadManifest} />;
+  //   }
+
+  //   if (data.length > 0 && !error) {
+  //     return (
+  //       <>
+  //         <ManifestDialog display={display} message={message} upload={uploadManifest} />
+  //         <AnalyticalTable columns={tableColumns} data={data} />
+  //         <FileUploader onChange={uploadManifest} hideInput>
+  //           <Button>Upload single file</Button>
+  //         </FileUploader>
+  //       </>
+  //     );
+  //   }
+
+  return (
+    // When same file uploaded twice, it doesnt display the error until refresh
+    <>
+      <ManifestDialog display={display} setDisplay={displayDialog} message={message} upload={uploadManifest} />
+      <AnalyticalTable columns={tableColumns} data={data} />
+      <FileUploader onChange={uploadManifest} hideInput>
+        <Button>Upload single file</Button>
+      </FileUploader>
+    </>
+  );
 }
--- client/src/components/Dialogs.jsx
+++ client/src/components/Dialogs.jsx
@@ -1,21 +1,13 @@
-import { Dialog, FileUploader, Button } from "@ui5/webcomponents-react"
+import { Dialog, FileUploader, Button } from '@ui5/webcomponents-react';
 
-
-function ManifestDialog({display, message, upload}){
-
-    // Consider conditional renders of the dialog, e.g if you have an error dont include an upload button
-    return(
-        <>
-        <Dialog open={display} headerText="Message">
-            <FileUploader hideInput onChange={upload}>
-                {message}
-            <Button>
-                Upload single file
-            </Button>
-            </FileUploader>
-            </Dialog>
-            </>
-    )
+function ManifestDialog({ display, setDisplay, message, upload }) {
+  return (
+    <Dialog open={display} headerText="Message" footer={<Button onClick={setDisplay}>Close</Button>}>
+      <FileUploader hideInput onChange={upload}>
+        {message}
+      </FileUploader>
+    </Dialog>
+  );
 }
 
-export {ManifestDialog}
+export { ManifestDialog };
