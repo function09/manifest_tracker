bab6d0aa019179144e57059f9d4c80c9bb3bbb4e
houssine <houssine199222@gmail.com>
2024-04-15 21:11:13 -0400

    Clean up analytical table component

--- client/src/components/AnalyticalTable.jsx
+++ client/src/components/AnalyticalTable.jsx
@@ -1,44 +1,50 @@
 import '@ui5/webcomponents-icons/dist/AllIcons.js';
 import { useEffect, useState, useRef } from 'react';
-import { AnalyticalTable, FlexBox, Button, FileUploader, Input } from '@ui5/webcomponents-react';
-import { ItemsDialog, ManifestDialog } from './Dialogs';
-import {
-  fetchManifests,
-  fetchItems,
-  uploadManifest,
-  deleteManifests,
-  editMaterialDocument,
-  login,
-} from '../networkRequests/fetchRequests';
+import { AnalyticalTable, FlexBox, Button, Input } from '@ui5/webcomponents-react';
+import { ItemsDialog } from './Dialogs';
+import { fetchManifests, fetchItems, deleteManifests, editMaterialDocument } from '../networkRequests/fetchRequests';
 import FileUpload from './FileUpload';
 
-export default function DocumentTable({ data, setData, loginSession, handleFileUpload }) {
+export default function DocumentTable({ manifestData, setManifestData, loginSession, handleFileUpload }) {
   // NEED TO HANDLE ERRORS ON ALL COMPONENTS, DISPLAY ITEM DATA,UPDATE, AND DELETE FUNCTIONS
-  // const [data, setData] = useState([]);
-  // const [message, setMessage] = useState('');
-  // const [isEmpty, setIsEmpty] = useState(true);
-  // const [display, setDisplay] = useState(false);
-  // const [isOpen, setIsOpen] = useState(false);
-  // const [error, setError] = useState(null);
+  const [itemData, setItemData] = useState([]);
+
+  const [isOpen, setIsOpen] = useState(false);
   const [UUID, setUUID] = useState(null);
-  // const [docNumber, setDocNumber] = useState(null);
-  // const [items, setItems] = useState([]);
   const [editingRowId, setEditingRowId] = useState(null);
   const [editValue, setEditValue] = useState('');
   const editValueRef = useRef('');
-
-  // // Change this to displayError and it's dependencies
-  // function displayDialog() {
-  //   if (display === true) setDisplay(false);
-  // }
 
   async function displayData() {
     try {
       if (loginSession) {
         const data = await fetchManifests();
-        setData(data);
+        setManifestData(data);
       } else {
-        setData([]);
+        setManifestData([]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function openItemsDialog() {
+    setIsOpen(true);
+  }
+
+  function closeItemDialog() {
+    setIsOpen(false);
+  }
+
+  async function displayItems(UUID) {
+    openItemsDialog();
+    try {
+      const result = await fetchItems(UUID);
+      const items = await result.data.items;
+      if (loginSession) {
+        setItemData(items);
+      } else {
+        setItemData([]);
       }
     } catch (error) {
       console.log(error);
@@ -54,20 +60,9 @@ export default function DocumentTable({ data, setData, loginSession, handleFileU
     setUUID(UUID);
   }
 
-  // function toggleEditMode(UUID, initialValue) {
-  //   setEditingRowId(UUID);
-  //   setUUID(UUID);
-  //   editValueRef.current = initialValue;
-  // }
-
   function handleInputChange(event) {
     editValueRef.current = event.target.value;
   }
-
-  // function saveChanges() {
-  //   editMaterialDocument(UUID, editValueRef.current);
-  //   setEditingRowId(null);
-  // }
 
   // Handle validation errors
   async function saveChanges() {
@@ -80,7 +75,7 @@ export default function DocumentTable({ data, setData, loginSession, handleFileU
         console.log('Validation errors:', errorData.message);
       }
 
-      const newData = data.map((item) => {
+      const newData = manifestData.map((item) => {
         if (item.UUID === editingRowId) {
           return { ...item, materialDocNumber: editValueRef.current };
         }
@@ -88,33 +83,14 @@ export default function DocumentTable({ data, setData, loginSession, handleFileU
       });
 
       // Update data array with modified data
-      setData(newData);
+      setManifestData(newData);
 
       // Reset editing state
       setEditingRowId(null);
     } catch (error) {
       console.log(error);
     }
-
-    // Update the data array with the new material doc number
-
-    // // Update the state with the new data
-    // onUpdate(newData);
   }
-
-  // function displayItems(UUID) {
-  //   setUUID(UUID);
-  //   setIsOpen(true);
-  //   fetchItems(UUID, setDocNumber, setItems, setError);
-  // }
-
-  // function closeItemDisplay() {
-  //   setIsOpen(false);
-  // }
-
-  // useEffect(() => {
-  //   fetchManifests(setData, setMessage, setDisplay, setIsEmpty, setError);
-  // }, []);
 
   const tableColumns = [
     {
@@ -161,20 +137,12 @@ export default function DocumentTable({ data, setData, loginSession, handleFileU
       Cell: (props) => {
         const { original } = props.row;
         const { UUID } = original;
-        // console.log(original);
-        // function handleDisplayItems(UUID) {
-        //   displayItems(UUID);
-        // }
-
-        // function handleDelete(UUID) {
-        //   onDelete(UUID);
-        // }
 
         async function handleDelete(UUID) {
           try {
             const response = await deleteManifests(UUID);
 
-            setData((prevData) => prevData.filter((item) => item.UUID !== UUID));
+            setManifestData((prevData) => prevData.filter((item) => item.UUID !== UUID));
             return response;
           } catch (error) {
             console.log('An error occurred:', error);
@@ -199,34 +167,16 @@ export default function DocumentTable({ data, setData, loginSession, handleFileU
                     handleDelete(UUID);
                   }}
                 />
-                <Button icon="activity-items" />
+                <Button
+                  icon="activity-items"
+                  onClick={() => {
+                    displayItems(UUID);
+                  }}
+                />
               </>
             )}
           </FlexBox>
         );
-        // return (
-        //   <FlexBox>
-        //     {editingRowId === UUID ? (
-        //       <Button icon="save" onClick={saveChanges} />
-        //     ) : (
-        //       <>
-        //         <Button
-        //           icon="edit"
-        //           onClick={() => {
-        //             toggleEditMode(UUID);
-        //           }}
-        //         />
-        //         <Button
-        //           icon="delete"
-        //           onClick={() => {
-        //             handleDelete(UUID);
-        //           }}
-        //         />
-        //         <Button icon="activity-items" onClick={() => handleDisplayItems(UUID)} />
-        //       </>
-        //     )}
-        //   </FlexBox>
-        // );
       },
     },
   ];
@@ -234,20 +184,13 @@ export default function DocumentTable({ data, setData, loginSession, handleFileU
   return (
     // When same file uploaded twice, it doesnt display the error until refresh
     <>
-      {/* <ItemsDialog isOpen={isOpen} setIsOpen={closeItemDisplay} itemData={items} docNumber={docNumber} /> */}
       {loginSession && (
         <>
-          <AnalyticalTable columns={tableColumns} data={data} />
+          <AnalyticalTable columns={tableColumns} data={manifestData} />
           <FileUpload handleFileUpload={handleFileUpload} />
+          <ItemsDialog isOpen={isOpen} closeDialog={closeItemDialog} data={itemData} />
         </>
       )}
-
-      {/* <FileUploader
-        onChange={(event) => uploadManifest(event, setMessage, setDisplay, setIsEmpty, setError, setData)}
-        hideInput
-      >
-        <Button>Upload single file</Button>
-      </FileUploader> */}
     </>
   );
 }
