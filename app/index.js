352a052c83d1f8106db9a666531b2a8156d6f10c
houssine <houssine199222@gmail.com>
2024-04-24 00:11:12 -0400

    Remove secure property

--- app/index.js
+++ app/index.js
@@ -21,7 +21,6 @@ app.options("/api/v1/manifests", cors());
 app.use(
   cookieParser(process.env.SECRET, {
     sameSite: "lax",
-    secure: true,
   })
 );
 app.use(
@@ -33,7 +32,6 @@ app.use(
       maxAge: 60000 * 60,
       httpOnly: true,
       sameSite: "lax",
-      secure: true,
       domain: "manifest-tracker-api-production.up.railway.app",
     },
   })
